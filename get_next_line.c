3720cededba4f82d3a089972f6511a4a3cd260dd
chdonnat <donnatchris@live.fr>
2024-11-21 10:51:05 +0100

    norminette ok, projetc finished but now need testing

--- get_next_line.c
+++ get_next_line.c
@@ -12,107 +12,106 @@
 
 #include "get_next_line.h"
 
-size_t	ft_newline_in_buff(char **newline_pos, char **buff, char **line, char **remainder)
+size_t	ft_newline_in_buff(char **newline_pos, \
+	char **buff, char **line, char **remainder)
 {
 	size_t	eol_index;
 	char	*temp;
 
-
-			eol_index = *newline_pos - *buff;
-            temp = ft_strjoin(*line, *buff, eol_index + 1);
-            if (!temp)
-                return (0);
-            ft_free(1, *line);
-            *line = temp;
-            ft_free(1, *remainder);
-            *remainder = ft_strndup(*newline_pos + 1, ft_strlen(*newline_pos + 1));
-            return (1);
-
+	eol_index = *newline_pos - *buff;
+	temp = ft_strjoin(*line, *buff, eol_index + 1);
+	if (!temp)
+		return (0);
+	ft_free(1, *line);
+	*line = temp;
+	ft_free(1, *remainder);
+	*remainder = ft_strndup(*newline_pos + 1, ft_strlen(*newline_pos + 1));
+	return (1);
 }
 
-size_t ft_read(int fd, char **buff, char **line, char **remainder)
+size_t	ft_read(int fd, char **buff, char **line, char **remainder)
 {
-    size_t	n_read;
+	size_t	n_read;
 	size_t	eol_index;
-    char	*temp;
-    char	*newline_pos;
+	char	*temp;
+	char	*newline_pos;
 
-    while (1)
-    {
-        n_read = read(fd, *buff, BUFFER_SIZE);
-        if (n_read <= 0)
-            return (0);
-        (*buff)[n_read] = '\0';
-        newline_pos = ft_strchr(*buff, '\n');
-        if (newline_pos)
-        {
+	while (1)
+	{
+		n_read = read(fd, *buff, BUFFER_SIZE);
+		if (n_read <= 0)
+			return (0);
+		(*buff)[n_read] = '\0';
+		newline_pos = ft_strchr(*buff, '\n');
+		if (newline_pos)
+		{
 			return (ft_newline_in_buff(&newline_pos, buff, line, remainder));
-        }
-        temp = ft_strjoin(*line, *buff, n_read);
-        if (!temp)
-            return (0);
-        ft_free(1, *line);
-        *line = temp;
-    }
+		}
+		temp = ft_strjoin(*line, *buff, n_read);
+		if (!temp)
+			return (0);
+		ft_free(1, *line);
+		*line = temp;
+	}
 }
 
-
-char	*ft_return_cpy_remainder(char *line, char **newline_pos, char **remainder)
+char	*ft_return_cpy_remainder(char *line, \
+	char **newline_pos, char **remainder)
 {
-            char	*new_remainder;
+	char	*new_remainder;
 
-			line = ft_strndup(*remainder, *newline_pos - *remainder + 1);
-			if (!line)
-				return (NULL);
-            new_remainder = ft_strndup(*newline_pos + 1, ft_strlen(*newline_pos + 1));
-			if (!new_remainder)
-				return (line);
-            ft_free(1, *remainder);
-            *remainder = new_remainder;
-            return (line);
+	line = ft_strndup(*remainder, *newline_pos - *remainder + 1);
+	if (!line)
+		return (NULL);
+	new_remainder = ft_strndup(*newline_pos + 1, ft_strlen(*newline_pos + 1));
+	if (!new_remainder)
+		return (line);
+	ft_free(1, *remainder);
+	*remainder = new_remainder;
+	return (line);
 }
 
 char	*ft_line(int fd, char **buff, char **remainder)
 {
-    char	*newline_pos;
-    char	*line;
+	char	*newline_pos;
+	char	*line;
 
-    if (*remainder)
+	if (*remainder)
 	{
-        newline_pos = ft_strchr(*remainder, '\n');
-        if (newline_pos)
+		newline_pos = ft_strchr(*remainder, '\n');
+		if (newline_pos)
 		{
 			line = ft_return_cpy_remainder(line, &newline_pos, remainder);
 			return (line);
 		}
-        line = ft_strndup(*remainder, ft_strlen(*remainder));
-        ft_free(1, *remainder);
-        *remainder = NULL;
-    }
-    else
-        line = NULL;
-    if (ft_read(fd, buff, &line, remainder) == 0)
-    {
-        free(line);
-        return (NULL);
-    }
-    return (line);
+		line = ft_strndup(*remainder, ft_strlen(*remainder));
+		ft_free(1, *remainder);
+		*remainder = NULL;
+	}
+	else
+		line = NULL;
+	if (ft_read(fd, buff, &line, remainder) == 0)
+	{
+		free(line);
+		return (NULL);
+	}
+	return (line);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    char		*line;
-    char		*buff;
-    static char	*remainder = NULL;
+	char		*line;
+	char		*buff;
+	static char	*remainder = NULL;
 
-    if (fd < 0 || BUFFER_SIZE <= 0)
-        return (NULL);
-    buff = (char *)malloc(BUFFER_SIZE + 1);
-    if (!buff)
-        return (NULL);
-    line = ft_line(fd, &buff, &remainder);
-    ft_free(1, buff);
-    return (line);
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buff = (char *)malloc(BUFFER_SIZE + 1);
+	if (!buff)
+		return (NULL);
+	line = ft_line(fd, &buff, &remainder);
+	ft_free(1, buff);
+	return (line);
 }
 
 #include <stdio.h>
@@ -140,3 +139,4 @@ int main(void)
     }
     return (0);
 }
+
